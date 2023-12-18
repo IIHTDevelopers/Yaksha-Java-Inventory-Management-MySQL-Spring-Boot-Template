fb91170ed6d3b1e4b0407ef98000a4b6221501a2
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-18 14:54:40 +0530

    Update ProductServiceImpl.java

--- src/main/java/com/inventorymanagement/service/impl/ProductServiceImpl.java
+++ src/main/java/com/inventorymanagement/service/impl/ProductServiceImpl.java
@@ -2,33 +2,45 @@ package com.inventorymanagement.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.inventorymanagement.dto.ProductDTO;
 import com.inventorymanagement.service.ProductService;
 
+@Service
 public class ProductServiceImpl implements ProductService {
 
 	@Override
 	public List<ProductDTO> getAllProducts() {
+		// write your logic here
 		return null;
 	}
 
 	@Override
 	public ProductDTO getProductById(Long id) {
+		// write your logic here
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public ProductDTO createProduct(ProductDTO productDTO) {
+		// write your logic here
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public ProductDTO updateProduct(Long id, ProductDTO productDTO) {
+		// write your logic here
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public boolean deleteProduct(Long id) {
+		// write your logic here
 		return false;
 	}
 }
