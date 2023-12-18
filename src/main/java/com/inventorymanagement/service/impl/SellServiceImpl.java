b62ce3943325cd20f43b5d0fe0763788ef39433e
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-12-18 14:54:56 +0530

    Update SellServiceImpl.java

--- src/main/java/com/inventorymanagement/service/impl/SellServiceImpl.java
+++ src/main/java/com/inventorymanagement/service/impl/SellServiceImpl.java
@@ -3,28 +3,37 @@ package com.inventorymanagement.service.impl;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.inventorymanagement.dto.SellDTO;
 import com.inventorymanagement.service.SellService;
 
+@Service
 public class SellServiceImpl implements SellService {
 
 	@Override
-	public SellDTO getSellById(Long id) {
+	public List<SellDTO> getAllSells() {
+		// write your logic here
 		return null;
 	}
 
 	@Override
+	public SellDTO getSellById(Long id) {
+		// write your logic here
+		return null;
+	}
+
+	@Override
+	@Transactional
 	public SellDTO createSell(SellDTO sellDTO) {
+		// write your logic here
 		return null;
 	}
 
 	@Override
 	public Map<String, Integer> getProductsSoldInLastMonth() {
-		return null;
-	}
-
-	@Override
-	public List<SellDTO> getAllSells() {
+		// write your logic here
 		return null;
 	}
 
